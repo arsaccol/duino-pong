47634aedce9d57ffc6c0218a34b7435d6c43dada
Alexandre R. Saccol <arsaccol@gmail.com>
2019-04-07 18:56:07 -0300

    Program just increments some characters and shows them in the middle of the display

---
+++ pong.ino
@@ -0,0 +1,53 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 32
+
+#define OLED_RESET 4
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+void printCharacter(char character)
+{
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(SCREEN_WIDTH / 2 - 1, SCREEN_HEIGHT / 2 - 1);
+    display.println(String(character));
+}
+
+char incrementing_character = 'A';
+
+void setup()
+{
+    Serial.begin(9600);
+
+    if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+    {
+        Serial.println(F("Couldn't begin serial"));
+        for(;;);
+    }
+
+    display.display();
+    delay(500);
+
+}
+
+void loop()
+{
+    if(incrementing_character > 'z')
+        incrementing_character = 'A';
+
+    display.clearDisplay();
+    // Actual display code here
+    
+    printCharacter(incrementing_character);
+
+    // End actual display code
+    display.display();
+
+    delay(200);
+    ++incrementing_character;
+}
