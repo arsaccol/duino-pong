5c775a71e8f3c286c367d117d45030abefdacae3
Alexandre R. Saccol <arsaccol@gmail.com>
2019-04-21 18:25:01 -0300

    Correctly sending codes to Arduino

--- duino_serial.py
+++ duino_serial.py
@@ -7,6 +7,7 @@
 import serial
 import time
 import datetime
+import keyboard
 
 duino = serial.Serial('COM3', 9600)
 time.sleep(1)
@@ -14,9 +15,17 @@ time.sleep(1)
 while True:
     # use this encode function so the stupid Python strings
     # are converted into proper ASCII ones
-    duino.write((str(datetime.datetime.now().isoformat(' ', 'seconds')) + '\n').encode())
+    #duino.write((str(datetime.datetime.now().isoformat(' ', 'seconds')) + '\n').encode())
     #duino.write(('lalala' + 'n').encode())
-    time.sleep(1)
-    print(datetime.datetime.now())
+
+    #for key_press in keyboard._pressed_events:
+    #    print(str(key_press))
+    #    duino.write(bytes(key_press))
+
+    #duino.write(bytes('x')str.encode('ascii'))
+    duino.write(bytearray([ord('a'), ord('b'), ord('c'), ord('d'), ord('5')]))
+    #duino.write(bytearray([ord('a')]))
+
+
 
 duino.close()
---
+++ pygame_playaround.py
@@ -0,0 +1,31 @@
+import sys, pygame
+import serial
+import time
+
+def main():
+    size = width, height = 320, 240
+
+    screen = pygame.display.set_mode(size)
+
+    duino = serial.Serial('COM3', 9600)
+    time.sleep(1)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+            #if event.type == pygame.MOUSEMOTION:
+            #    x,y = pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1]
+
+            #    print("Mouse position: ", str(x), str(y))
+            if event.type == pygame.KEYDOWN:
+                print(event.key)
+                duino.write(bytearray([event.key]))
+
+
+
+    
+    
+    
+
+if __name__ == '__main__':
+    main()
